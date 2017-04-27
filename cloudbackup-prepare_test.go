5d89d4f528ef828b8e4e60ffd32ffc59f37674b5
abednarik <alejandro.bednarik@gmail.com>
2017-04-26 22:13:23 -0300

    Change the way we parse flags to facilitate unit tests.

--- cloudbackup-prepare_test.go
+++ cloudbackup-prepare_test.go
@@ -25,6 +25,12 @@ func GenerateRandomString(s int) (string, error) {
 	return "/tmp/" + base64.URLEncoding.EncodeToString(b), err
 }
 
+func resetArguments() {
+	inputFile = ""
+	outputFile = ""
+	encryptionKey = ""
+}
+
 func Test_validateInputFile(t *testing.T) {
 	type args struct {
 		filename string
@@ -46,7 +52,7 @@ func Test_validateInputFile(t *testing.T) {
 			args: args{
 				filename: `tesfiles/foo.z`,
 			},
-			expect: fmt.Errorf("Input file doesn't exists"),
+			expect: fmt.Errorf("Backup file doesn't exists"),
 		},
 		{
 			name: "Directory as input file",
@@ -60,7 +66,7 @@ func Test_validateInputFile(t *testing.T) {
 			args: args{
 				filename: ``,
 			},
-			expect: fmt.Errorf("Input file is mandatory"),
+			expect: fmt.Errorf("Backup file doesn't exists"),
 		},
 	}
 	for _, tt := range tests {
@@ -103,13 +109,6 @@ func Test_validateOutputFile(t *testing.T) {
 			},
 			expect: fmt.Errorf("Output file exists tesfiles/dummy.sql. Please remove it"),
 		},
-		{
-			name: "Missing output file",
-			args: args{
-				filename: ``,
-			},
-			expect: fmt.Errorf("Output file is mandatory"),
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -149,7 +148,7 @@ func Test_prepareBackupFile(t *testing.T) {
 				encryptionKey: `kpySdc2vfHL_4WebUstA29fRFacKis8LZRbLqFFY0HM=`,
 				output:        `/tmp/backup.json`,
 			},
-			expect: fmt.Errorf("Input file doesn't exists"),
+			expect: fmt.Errorf("Backup file doesn't exists"),
 		},
 		{
 			name: "Invalid encryption key",
@@ -158,7 +157,7 @@ func Test_prepareBackupFile(t *testing.T) {
 				encryptionKey: `InvalidKeyTest`,
 				output:        `/tmp/mysqldump.sql`,
 			},
-			expect: fmt.Errorf("Fail to decrypt file tesfiles/mysqldump_sql.z using InvalidKeyTest: illegal base64 data at input byte 12"),
+			expect: fmt.Errorf("Fail to decrypt file tesfiles/mysqldump_sql.z using InvalidKeyTest. Be sure encryption key is correct"),
 		},
 		{
 			name: "Directory as input file",
@@ -169,10 +168,85 @@ func Test_prepareBackupFile(t *testing.T) {
 			},
 			expect: fmt.Errorf("Expecting a file but tesfiles is a directory"),
 		},
+		{
+			name: "Plain input file",
+			args: args{
+				filename:      `tesfiles/dummy.sql`,
+				encryptionKey: `InvalidKeyTest`,
+				output:        `/tmp/backup_dir.sql`,
+			},
+			expect: fmt.Errorf("Fail opening zlib reader: unexpected EOF"),
+		},
+		{
+			name: "Incorrect output file",
+			args: args{
+				filename:      `tesfiles/mysqldump_sql.z`,
+				encryptionKey: `kpySdc2vfHL_4WebUstA29fRFacKis8LZRbLqFFY0HM=`,
+				output:        `/missing/dir/file.sql`,
+			},
+			expect: fmt.Errorf("Fail to create /missing/dir/file.sql: open /missing/dir/file.sql: no such file or directory"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			result := prepareBackupFile(tt.args.filename, tt.args.encryptionKey, tt.args.output)
+			if !reflect.DeepEqual(result, tt.expect) {
+				t.Errorf("Expecting %s got %s", tt.expect, result)
+			}
+		})
+	}
+}
+
+func Test_parseArgs(t *testing.T) {
+	type args struct {
+		opts []string
+	}
+	tests := []struct {
+		name   string
+		args   args
+		expect []string
+	}{
+		{
+			name: "Validate backup-file argument",
+			args: args{
+				opts: []string{"cmd", "--backup-file", "infile.sql"},
+			},
+			expect: []string{"infile.sql", "", ""},
+		},
+		{
+			name: "Validate backup-file and output-file arguments",
+			args: args{
+				opts: []string{"cmd", "--backup-file", "infile.sql", "--output-file", "outfile.sql"},
+			},
+			expect: []string{"infile.sql", "outfile.sql", ""},
+		},
+		{
+			name: "Validate backup-file and output-file and encryption-key arguments",
+			args: args{
+				opts: []string{"cmd", "--backup-file", "file.json.z", "--output-file", "file.json", "--encryption-key", "XXXXXXXXXX"},
+			},
+			expect: []string{"file.json.z", "file.json", "XXXXXXXXXX"},
+		},
+		{
+			name: "Validate output-file argument",
+			args: args{
+				opts: []string{"cmd", "--output-file", "out.sql"},
+			},
+			expect: []string{"", "out.sql", ""},
+		},
+		{
+			name: "Validate encryptionKey argument",
+			args: args{
+				opts: []string{"cmd", "-e", "SOMERANDOMSTRING"},
+			},
+			expect: []string{"", "", "SOMERANDOMSTRING"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resetArguments()
+			_ = parseArgs(tt.args.opts)
+			result := []string{inputFile, outputFile, encryptionKey}
 			if !reflect.DeepEqual(result, tt.expect) {
 				t.Errorf("Expecting %s got %s", tt.expect, result)
 			}
